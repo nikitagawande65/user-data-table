e144805c33d882c7c035d2de1bb51d943baec507
nikitagawande65 <38561323+nikitagawande65@users.noreply.github.com>
2021-08-18 11:02:08 +0530

    Add files via upload

---
+++ user-table/src/app/dialog-box/dialog-box.component.ts
@@ -0,0 +1,36 @@
+//dialog-box.component.ts
+import { Component, Inject, Optional } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface UsersData {
+  name: string;
+  id: number;
+}
+
+
+@Component({
+  selector: 'app-dialog-box',
+  templateUrl: './dialog-box.component.html',
+})
+export class DialogBoxComponent {
+
+  action:string;
+  local_data:any;
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogBoxComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: UsersData) {
+    this.local_data = {...data};
+    this.action = this.local_data.action;
+  }
+
+  onSubmit(){
+    this.dialogRef.close({event:this.action,data:this.local_data});
+  }
+
+  closeDialog(){
+    this.dialogRef.close({event:'Cancel'});
+  }
+
+}
+
